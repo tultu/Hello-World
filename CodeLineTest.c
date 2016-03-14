a02c9a154a144e11d5db38cfed50d22ff0243368
tultu <tultu@163.com>
2016-03-14 09:08:54 +0800

    Added files via upload

---
+++ CodeLineTest.c
@@ -0,0 +1,16 @@
+void main()
+{
+	int a;
+	int b; int c;
+	a = 0;
+	b = 0;// init b
+	
+	// init c
+	c = 0;
+	
+	/* assign value to b if a is zero */
+	if( a == 0 ) {
+		b = 1; /* assign value */
+	}
+	
+}
